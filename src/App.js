4eddeed0e997f7cb8a15ee7aa61ae3ca3fad1211
NicoaraMihai <mihainicoara110@gmail.com>
2022-04-16 15:21:27 +0300

    .sme

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TicTacToe from "./TicTacToe";
+  
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TicTacToe />
+      </header>
+    </div>
+  );
+}
+
+export default App;
